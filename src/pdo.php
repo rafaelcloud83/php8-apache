45517b1ab034e506f580faf8331b2627616acddf
Rafael Ciriaco da Silva <79068831+rafaelcloud83@users.noreply.github.com>
2021-09-12 23:40:23 -0300

    Update pdo.php

--- src/pdo.php
+++ src/pdo.php
@@ -1,6 +1,7 @@
 <?php
 try {
-  $conn = new PDO('mysql:host=db;dbname=banco', 'root', 'root');
+  $conn = new PDO('mysql:host=db;dbname=nomeBanco', 'root', 'root');
+  //substituir o nomeBanco pelo seu banco
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo 'Conectado';
 } catch(PDOException $e) {
