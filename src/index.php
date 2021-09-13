c54c3af9466bfa3f0bbbe72f8cfaf072867e15c1
Rafael Ciriaco da Silva <79068831+rafaelcloud83@users.noreply.github.com>
2021-09-12 23:39:07 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,17 +1,4 @@
 <?php
-//These are the defined authentication environment in the db service
-
-// The MySQL service named in the docker-compose.yml.
-//$host = 'db';
-
-// Database use name
-//$user = 'root';
-
-//database user password
-//$pass = 'root';
-
-// check the MySQL connection status
-//$conn = new mysqli($host, $user, $pass);
 $conn = mysqli_connect('db','root','root');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
